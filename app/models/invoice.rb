e24d4ebb155e50b475e7266dd2cca6b857ef915b
Romulo <romulogomes93@gmail.com>
2022-07-04 09:55:13 -0300

    teste nova conta monkey

--- app/models/invoice.rb
+++ app/models/invoice.rb
@@ -9,7 +9,8 @@ class Invoice < ApplicationRecord
   def generate_pdf
     return if previous_changes.key?('pdfmonkey_id')
 
-    template_id = 'C5DE6D11-280A-443D-BBE0-93CFF095BCCE'
+    #template_id = 'C5DE6D11-280A-443D-BBE0-93CFF095BCCE'
+    template_id = '3B8FF054-6C90-49D6-96AF-5CF1C5CADB27'
     data = {
       invoice_number: number,
       invoice_date: date,
--- config/initializers/pdfmonkey.rb
+++ config/initializers/pdfmonkey.rb
@@ -1,3 +1,4 @@
 Pdfmonkey.configure do |config|
-  config.private_key = '7aaYZc56uy1jFjsbyjpy'
+  #config.private_key = '7aaYZc56uy1jFjsbyjpy'
+  config.private_key = 'nvKsPPucoq3ZgzJunidC'
 end
